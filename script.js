ac9f5c09c4920c4cae601cb12c9d74419b885ac4
Dylan Depass <dylandepass@gmail.com>
2021-09-02 14:26:48 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+window.adobeid = {
+      client_id: 'hlx-ims-auth-demo-stg',
+      scope: 'AdobeID,openid',
+      locale: 'en_US',
+      environment: 'stg1',
+      useLocalStorage: false,
+      onAccessToken: function (tokenInformation) {
+        document.querySelector('.token').html = JSON.stringify(tokenInformation);
+      },
+      onReauthAccessToken: function (reauthTokenInformation) {
+      },
+      onError: function (error) {
+        document.querySelector('.token').html = JSON.stringify(error);
+      },
+      onAccessTokenHasExpired: function() {
+      },
+      onReady: function(appState) {
+      }
+};

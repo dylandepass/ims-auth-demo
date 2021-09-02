0f79215b6b466abbe18cdf5acd2087921a5d0457
Dylan Depass <dyland@adobe.com>
2021-09-02 15:41:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
-const status = document.querySelector('.status');
-const token = document.querySelector('.token');
-
-window.adobeid = {
-      client_id: 'hlx-ims-auth-demo-stg',
-      scope: 'AdobeID,openid',
-      locale: 'en_US',
-      environment: 'stg1',
-      redirect_uri: 'https://main--ims-auth-demo--dylandepass.hlx3.page/',
-      useLocalStorage: false,
-      onAccessToken: function (tokenInformation) {
-            status.textContent = "Status: Logged In";
-            token.textContent = `Token: ${tokenInformation.token}`;
-      },
-      onReauthAccessToken: function (reauthTokenInformation) {
-      },
-      onError: function (error) {
-            status.textContent = "Status: Error";
-            token.textContent = JSON.stringify(error);
-      },
-      onAccessTokenHasExpired: function() {
-            status.textContent = "Status: Expired";
-      },
-      onReady: function(appState) {
-            console.log('On Ready');
+window.addEventListener('load', function () {
+      const status = document.querySelector('.status');
+      const token = document.querySelector('.token');
+      
+      window.adobeid = {
+            client_id: 'hlx-ims-auth-demo-stg',
+            scope: 'AdobeID,openid',
+            locale: 'en_US',
+            environment: 'stg1',
+            redirect_uri: 'https://main--ims-auth-demo--dylandepass.hlx3.page/',
+            useLocalStorage: false,
+            onAccessToken: function (tokenInformation) {
+                  status.textContent = "Status: Logged In";
+                  token.textContent = `Token: ${tokenInformation.token}`;
+            },
+            onReauthAccessToken: function (reauthTokenInformation) {
+            },
+            onError: function (error) {
+                  status.textContent = "Status: Error";
+                  token.textContent = JSON.stringify(error);
+            },
+            onAccessTokenHasExpired: function() {
+                  status.textContent = "Status: Expired";
+            },
+            onReady: function(appState) {
+                  console.log('On Ready');
+            }
+      };
+      
+      function login() {
+            adobeIMS.signIn();
       }
-};
-
-function login() {
-      adobeIMS.signIn();
-}
-
-function logout() {
-      adobeIMS.signOut();
-      status.textContent = "Status: Logged Out";
-}
+      
+      function logout() {
+            adobeIMS.signOut();
+            status.textContent = "Status: Logged Out";
+      }
+})

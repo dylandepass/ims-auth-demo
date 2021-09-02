4eb1c7304d412bdacebf0fcd973c951639d3e9a8
Dylan Depass <dyland@adobe.com>
2021-09-02 15:27:59 -0400

    Update

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ window.adobeid = {
       scope: 'AdobeID,openid',
       locale: 'en_US',
       environment: 'stg1',
+      redirect_uri: 'https://main--ims-auth-demo--dylandepass.hlx3.page/extension.html',
       useLocalStorage: false,
       onAccessToken: function (tokenInformation) {
-            console.log('Got AccessToken');
-        document.querySelector('.token').textContent = JSON.stringify(tokenInformation);
+            console.log('Got AccessToken ' + JSON.stringify(tokenInformation));
+            document.querySelector('.token').textContent = JSON.stringify(tokenInformation);
       },
       onReauthAccessToken: function (reauthTokenInformation) {
       },
       onError: function (error) {
             console.log('Got Error');
-        document.querySelector('.token').html = JSON.stringify(error);
+            document.querySelector('.token').html = JSON.stringify(error);
       },
       onAccessTokenHasExpired: function() {
       },
@@ -20,3 +21,13 @@ window.adobeid = {
             console.log('On Ready')
       }
 };
+
+function login() {
+adobeIMS.signIn({
+      test: 1,
+      }, { say: 'hello' });
+}
+
+function logout() {
+      adobeIMS.signOut();
+}

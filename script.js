f40db235ff701cf6805cc8ad69f983849e4e0add
Dylan Depass <dyland@adobe.com>
2021-09-02 15:55:23 -0400

    Update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function login() {
-      adobeIMS.signIn();
+      window.adobeIMS.signIn();
 }
 
 function logout() {
@@ -7,32 +7,32 @@ function logout() {
       status.textContent = "Status: Logged Out";
 }
 
-window.addEventListener('load', function () {
-      const status = document.querySelector('.status');
-      const token = document.querySelector('.token');
-      
-      window.adobeid = {
-            client_id: 'hlx-ims-auth-demo-stg',
-            scope: 'AdobeID,openid',
-            locale: 'en_US',
-            environment: 'stg1',
-            redirect_uri: 'https://main--ims-auth-demo--dylandepass.hlx3.page/',
-            useLocalStorage: false,
-            onAccessToken: function (tokenInformation) {
-                  status.textContent = "Status: Logged In";
-                  token.textContent = `Token: ${tokenInformation.token}`;
-            },
-            onReauthAccessToken: function (reauthTokenInformation) {
-            },
-            onError: function (error) {
-                  status.textContent = "Status: Error";
-                  token.textContent = JSON.stringify(error);
-            },
-            onAccessTokenHasExpired: function() {
-                  status.textContent = "Status: Expired";
-            },
-            onReady: function(appState) {
-                  console.log('On Ready');
-            }
-      };
-})
+const status = document.querySelector('.status');
+const token = document.querySelector('.token');
+
+window.adobeid = {
+      client_id: 'hlx-ims-auth-demo-stg',
+      scope: 'AdobeID,openid',
+      locale: 'en_US',
+      environment: 'stg1',
+      redirect_uri: 'https://main--ims-auth-demo--dylandepass.hlx3.page/',
+      useLocalStorage: false,
+      onAccessToken: function (tokenInformation) {
+            status.textContent = "Status: Logged In";
+            token.textContent = `Token: ${tokenInformation.token}`;
+      },
+      onReauthAccessToken: function (reauthTokenInformation) {
+      },
+      onError: function (error) {
+            status.textContent = "Status: Error";
+            token.textContent = JSON.stringify(error);
+      },
+      onAccessTokenHasExpired: function() {
+            status.textContent = "Status: Expired";
+      },
+      onReady: function(appState) {
+            console.log('On ');
+            console.log('On Ready ' + JSON.stringify(appState));
+            token.textContent = `Token: ${appState}`;
+      }
+};
